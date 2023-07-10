fe612eb3b73850aabbb7b07d82cf9e95a4a06497
Matheus Soares <111543203+matheussoares1@users.noreply.github.com>
2023-07-10 08:40:06 -0300

    Update main.py

--- Sistema para ACADEMIAS/main.py
+++ Sistema para ACADEMIAS/main.py
@@ -8,6 +8,10 @@ from tkinter import filedialog as fd
 # importando pillow
 from PIL import ImageTk, Image
 
+# tk calendar
+from tkcalendar import Calendar, DateEntry
+from datetime import date
+
 
 # cores
 co0 = "#2e2d2b"  # Preta
@@ -17,7 +21,6 @@ co3 = "#00a095"  # Verde
 co4 = "#403d3d"   # letra
 co6 = "#003452"   # azul
 co7 = "#ef5350"   # vermelha
-
 co6 = "#038cfc"   # azul
 co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # + verde
@@ -65,16 +68,17 @@ def cadastrar():
 
 #Funcao para adicionar Personais e Turmas
 def adicionar():
-	frame_tabela_personais = Frame(frame_tabela, width=400, height=200, bg=co0)
+	
+	frame_tabela_personais = Frame(frame_tabela, width=400, height=200, bg=co1)
 	frame_tabela_personais.grid(row=0, column=0, pady=0, padx=20, sticky=NSEW)
 
-	frame_tabela_divisao = Frame(frame_tabela, width=40, height=200, bg=co1)
+	frame_tabela_divisao = Frame(frame_tabela, width=15, height=200, bg=co1)
 	frame_tabela_divisao.grid(row=0, column=1, pady=0, padx=10, sticky=NSEW)
 
-	frame_tabela_turmas = Frame(frame_tabela, width=300, height=200, bg=co0)
+	frame_tabela_turmas = Frame(frame_tabela, width=320, height=200, bg=co1)
 	frame_tabela_turmas.grid(row=0, column=2, pady=0, padx=10, sticky=NSEW)
 
-	#INPUTS
+	#Detalhes dos Personais inputs
 
 	l_nome = Label(frame_detalhes, text="Nome do(a) Personal: ", height=1, anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
 	l_nome.place(x=7, y=10)
@@ -102,9 +106,10 @@ def adicionar():
 
 	botao_deletar = Button(frame_detalhes, anchor=CENTER, text="Deletar".upper(), width=10, overrelief=RIDGE, font=('Ivy 7 bold'), bg=co7, fg=co1)
 	botao_deletar.place(x=327, y=145)
+
 	
 	def mostrar_personais():
-		app_nome = Label(frame_tabela_curso, text="Tabela de Personais", height=1,pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+		app_nome = Label(frame_tabela_personais, text="Tabela de Personais", height=1,pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 		app_nome.grid(row=0, column=0, padx=0, pady=10, sticky=NSEW)
 		
 		# creating a treeview with dual scrollbars
@@ -114,42 +119,139 @@ def adicionar():
 
 		global tree_personais
 
-		tree_personais = ttk.Treeview(frame_tabela_curso, selectmode="extended",columns=list_header, show="headings")
+		tree_personais = ttk.Treeview(frame_tabela_personais, selectmode="extended", columns=list_header, show="headings")
 
  		# vertical scrollbar
-		vsb = ttk.Scrollbar(frame_tabela_curso, orient="vertical", command=tree_personal.yview)
+		vsb = ttk.Scrollbar(frame_tabela_personais, orient="vertical", command=tree_personais.yview)
 		# horizontal scrollbar
-		hsb = ttk.Scrollbar(frame_tabela_curso, orient="horizontal", command=tree_personal.xview)
+		hsb = ttk.Scrollbar(frame_tabela_personais, orient="horizontal", command=tree_personais.xview)
 
-		tree_personal.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
-		tree_personal.grid(column=0, row=1, sticky='nsew')
+		tree_personais.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+		tree_personais.grid(column=0, row=1, sticky='nsew')
 		vsb.grid(column=1, row=1, sticky='ns')
 		hsb.grid(column=0, row=2, sticky='ew')
-		frame_tabela_curso.grid_rowconfigure(0, weight=12)
+		frame_tabela_personais.grid_rowconfigure(0, weight=12)
 
-		hd=["nw","nw","e","e"]
-		h=[30,150,80,60]
-		n=0
-
+		hd = ["nw","nw","e","e"]
+		h = [30,150,150,60]
+		n = 0
+ 
 		for col in list_header:
-   			tree_personal.heading(col, text=col.title(), anchor=NW)
-   			# adjust the column's width to the header string
-   			tree_personal.column(col, width=h[n],anchor=hd[n])
-
-   			n+=1
+			tree_personais.heading(col, text=col.title(), anchor=NW)
+			# adjust the column's width to the header string
+			tree_personais.column(col, width=h[n], anchor=hd[n])
+			n+=1
 
 		for item in df_list:
-   			tree_personais.insert('', 'end', values=item)
-
+			tree_personais.insert('', 'end', values=item)
+			
 	mostrar_personais()
+
+	#Linha divisória-------------------
+
+	l_linha = Label(frame_detalhes, relief=GROOVE, text='h', width=1, height=100, anchor=NW, font=('Ivy 1'), bg=co0, fg=co0)
+	l_linha.place(x=459, y=10)
+
+	l_linha = Label(frame_detalhes, relief=GROOVE, text='h', width=1, height=100, anchor=NW, font=('Ivy 1'), bg=co1, fg=co0)
+	l_linha.place(x=458, y=10)
+
+
+	#Linha divisória tabela-------------------
+
+	l_linha = Label(frame_tabela_divisao, relief=GROOVE, text='h', width=1, height=100, anchor=NW, font=('Ivy 1'), bg=co0, fg=co0)
+	l_linha.place(x=1, y=10)
+
+	l_linha = Label(frame_tabela_divisao, relief=GROOVE, text='h', width=1, height=100, anchor=NW, font=('Ivy 1'), bg=co1, fg=co0)
+	l_linha.place(x=0, y=10)
+
+
+	#Detalhes das Turmas INPUTS
+
+
+	l_nome_turma = Label(frame_detalhes, text="Nome da Turma: ", height=1, anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+	l_nome_turma.place(x=480, y=10)
+	enome_turma = Entry(frame_detalhes, width=35, justify='left', relief='solid')
+	enome_turma.place(x=480, y=35)
+
+
+	l_personalnome = Label(frame_detalhes, text="Personal Responsavel: ", height=1, anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+	l_personalnome.place(x=480, y=65)
+
+	#PEGANDO OS PERSONAIS
+	personais = ["Oscar", "Rafael"]
+	personal = []
+
+	for i in personais:
+		personal.append(i)
+
+	epersonal_nome = ttk.Combobox(frame_detalhes, width=20, font=('Ivy 8 bold'))
+	epersonal_nome['values'] = (personal)
+	epersonal_nome.place(x=480, y=90)
+
+
+	l_datainicio = Label(frame_detalhes, text="Data de Início: ", height=1, anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+	l_datainicio.place(x=480, y=120)
+	edata_inicio = DateEntry(frame_detalhes, width=10, background='darkblue', foreground='white', borderwidht=2, year=2020)
+	edata_inicio.place(x=480, y=145)
+
+
+	# Botões
+
+	botao_carregar_turmas = Button(frame_detalhes, anchor=CENTER, text="Salvar".upper(), width=10, overrelief=RIDGE, font=('Ivy 7 bold'), bg=co3, fg=co1)
+	botao_carregar_turmas.place(x=610, y=125)
+
+	botao_atualizar_turmas = Button(frame_detalhes, anchor=CENTER, text="Atualizar".upper(), width=10, overrelief=RIDGE, font=('Ivy 7 bold'), bg=co6, fg=co1)
+	botao_atualizar_turmas.place(x=720, y=125)
+
+	botao_deletar_turmas = Button(frame_detalhes, anchor=CENTER, text="Deletar".upper(), width=10, overrelief=RIDGE, font=('Ivy 7 bold'), bg=co7, fg=co1)
+	botao_deletar_turmas.place(x=665, y=160)
+
+
+
+
+	def mostrar_turmas():
+		app_nome = Label(frame_tabela_turmas, text="Tabela de Turmas", height=1,pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+		app_nome.grid(row=0, column=0, padx=0, pady=10, sticky=NSEW)
+		
+		# creating a treeview with dual scrollbars
+		list_header = ['ID','Nome','Personal','Início']
+
+		df_list = []
+
+		global tree_turmas
+
+		tree_turmas = ttk.Treeview(frame_tabela_turmas, selectmode="extended", columns=list_header, show="headings")
+
+ 		# vertical scrollbar
+		vsb = ttk.Scrollbar(frame_tabela_turmas, orient="vertical", command=tree_turmas.yview)
+		# horizontal scrollbar
+		hsb = ttk.Scrollbar(frame_tabela_turmas, orient="horizontal", command=tree_turmas.xview)
+
+		tree_turmas.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+		tree_turmas.grid(column=0, row=1, sticky='nsew')
+		vsb.grid(column=1, row=1, sticky='ns')
+		hsb.grid(column=0, row=2, sticky='ew')
+		frame_tabela_turmas.grid_rowconfigure(0, weight=12)
+
+		hd = ["nw","nw","e","e"]
+		h = [30,120,90,60]
+		n = 0
+ 
+		for col in list_header:
+			tree_turmas.heading(col, text=col.title(), anchor=NW)
+			# adjust the column's width to the header string
+			tree_turmas.column(col, width=h[n], anchor=hd[n])
+			n+=1
+
+		for item in df_list:
+			tree_turmas.insert('', 'end', values=item)
+			
+	mostrar_turmas()
 
 
 #Funcao para Salvar
 def salvar():
 	print('Salvar')
-
-
-
 
 
 #Funcao de controle
@@ -198,14 +300,14 @@ app_img_cadastro = ImageTk.PhotoImage(app_img_cadastro)
 app_cadastro = Button(frame_dados, command=lambda:control('cadastro'), image=app_img_cadastro, text=" Cadastro", width=100, compound=LEFT, overrelief=RIDGE, font=('Ivy 11'), bg=co1, fg=co0)
 app_cadastro.place(x=10, y=30)
 
-#Atualizar
+#Adicionar
 app_img_adicionar = Image.open('add.png')
 app_img_adicionar = app_img_adicionar.resize((18,18))
 app_img_adicionar = ImageTk.PhotoImage(app_img_adicionar)
 app_adicionar = Button(frame_dados, command=lambda:control('adicionar'), image=app_img_adicionar, text=" Adicionar", width=100, compound=LEFT, overrelief=RIDGE, font=('Ivy 11'), bg=co1, fg=co0)
 app_adicionar.place(x=140, y=30)
 
-#Deletar
+#Salvar
 app_img_salvar = Image.open('save.png')
 app_img_salvar = app_img_salvar.resize((18,18))
 app_img_salvar = ImageTk.PhotoImage(app_img_salvar)
@@ -213,7 +315,6 @@ app_salvar = Button(frame_dados, command=lambda:control('salvar'), image=app_img
 app_salvar.place(x=270, y=30)
 
 
-
-
 # Executando a janela
 janela.mainloop()
+ 
